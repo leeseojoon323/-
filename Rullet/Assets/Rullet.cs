c1eec8dd4018af767179f859a8b4fad211218c62
leeseojoon323 <80864768+leeseojoon323@users.noreply.github.com>
2021-03-18 10:09:42 +0900

    .

---
+++ Rullet/Assets/Rullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float rotSpeed = 0.0f;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10.0f;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        if (Input.GetMouseButtonDown(1))
+        
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.987f;
+    }
+   
+}
